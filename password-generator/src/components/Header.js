08d3f25e3e250140e4f5732d5c25448074c822e1
skramer2329 <skramer2329@gmail.com>
2022-06-22 14:46:29 -0500

    Created initial files and folders

---
+++ password-generator/src/components/Header.js
@@ -0,0 +1,13 @@
+import "../css/Header.css"
+
+// The Header will only have the star icon for darkmode feature
+
+
+export default function Header(){
+    return(
+        <div>
+            <p>This is the header</p>
+            <p>The header will just hold the img for toggling darkmode</p>
+        </div>
+    )
+}
